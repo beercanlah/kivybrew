6f95a65131f3a1964b814f9ea15d6f53b54ee6cb
Markus Baden <markus.baden@gmail.com>
2014-05-20 22:19:56 +0800

    App reads temperature from Arduino

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
+from kivy.clock import Clock
 
 # For now path to ardumashtun is hard coded, sorry
 import sys
@@ -11,11 +12,17 @@ from ardumashtun import UnoMashtun
 
 class BrewControl(Widget):
     temperature = NumericProperty(0)
+    mashtun = UnoMashtun('/dev/tty.usbmodem1411')
+
+    def update(self, dt):
+        self.temperature = self.mashtun.temperature
 
 
 class BrewApp(App):
     def build(self):
-        return BrewControl()
+        control = BrewControl()
+        Clock.schedule_interval(control.update, 1.0)
+        return control
 
 
 if __name__ == '__main__':
