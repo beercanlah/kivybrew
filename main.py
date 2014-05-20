81685da03ff8328a3177e2b4bc062af12ee11a53
Markus Baden <markus.baden@gmail.com>
2014-05-20 22:33:52 +0800

    Can read and set pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, BooleanProperty
 from kivy.clock import Clock
 
 # For now path to ardumashtun is hard coded, sorry
@@ -12,10 +12,16 @@ from ardumashtun import UnoMashtun
 
 class BrewControl(Widget):
     temperature = NumericProperty(0)
+    pump_status = BooleanProperty(False)
+
     mashtun = UnoMashtun('/dev/tty.usbmodem1411')
 
     def update(self, dt):
         self.temperature = self.mashtun.temperature
+        self.pump_status = self.mashtun.pump
+
+    def toggle_pump(self):
+        self.mashtun.pump = not self.mashtun.pump
 
 
 class BrewApp(App):
